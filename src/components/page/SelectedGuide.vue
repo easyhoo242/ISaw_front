56c835787d6e8217d004afc8b2654024da7abaeb
Du Cheng <easyhoo2426@qq.com>
2022-04-02 20:30:49 +0800

    feat(home): 使用数据替换模板

--- src/components/page/SelectedGuide.vue
+++ src/components/page/SelectedGuide.vue
@@ -1,24 +1,24 @@
 <template>
   <Module class="module mt-3">
     <div class="title text-lg mx-2.5 pb-2.5 font-bold border-b-1">精选导读</div>
-    <div class="content mt-2 items-center flex flex-wrap">
+    <div class="content mt-4 items-center flex flex-wrap">
       <div
-        v-for="item in 6"
-        :key="item"
-        class="item h-1/2 w-1/3 border-b-1 border-r-1 p-15px text-center"
+        v-for="item in listArr"
+        :key="item.id"
+        class="item h-1/2 w-1/3 border-b-1 border-r-1 p-15px text-center dark:border-gray-300"
       >
-        <div class="item-title flex items-center justify-between pb-2">
-          <div class="title-left">教程笔记</div>
-          <div class="title-right">2017-10-26</div>
+        <div class="item-title flex items-center justify-between pb-2 dark:color">
+          <div class="title-left">{{ item.type }}</div>
+          <div class="title-right">{{ item.time }}</div>
         </div>
 
         <div class="item-content text-left">
           <div class="content-desc">
-            <a>Z-blogPHP常见问题答疑（最新整理202lorem Lorem ipsum dolor sit, amet consectetur</a>
+            <a>{{ item.title }}</a>
           </div>
 
           <div class="content-text mt-1.5">
-            最新整理了一下zblog程序的思路，把过去遇到的问题总结一下，都是一.最新整理了一下zblog程序的思路，把过去遇到的问题总结一下，都是一.
+            {{ item.content }}
           </div>
         </div>
       </div>
@@ -30,9 +30,45 @@
 import { defineComponent } from 'vue'
 import Icon from 'ant-design-vue'
 
+// interface IListType {
+//   id: number
+//   title: string
+//   time: string
+//   type: string
+//   content: string
+// }
+
 export default defineComponent({
   components: {
     Icon
+  },
+  setup() {
+    const list = [
+      '1\n教程笔记\n2017-10-26\nZ-blogPHP常见问题答疑（最新整理2021/08）\n最新整理了一下zblog程序的思路，把过去遇到的zblogphp问题总结一下，都是一...',
+      '2\n教程笔记\n2019-01-25\nzblog开启主题或插件显示“授权文件非法”的解决办法（支持ZBP1.7+）\n最近好些用户都在反应，说启用主题之后后台显示“授权文件非法”，导致主题无法使用，如图...',
+      '3\n互联网\n2019-09-25\n网站打开之后，主题/插件显示错误的解决办法，适用于各种BUG。\n标题党怪我咯？？？这篇文章的主要目的不是真的能解决所有问题，是为了让问题具体化，按照...',
+      '4\n教程笔记\n2019-07-12\nzblog备份的网站及数据库怎么恢复？\n很多人可能会安装zblog程序，但是未必会恢复数据，很多种情况我们都会需要把网站打包...',
+      '5\n教程笔记\n2020-04-08\nzblog怎么在移动端显示/隐藏侧栏模块\n关于zblog主题模板手机移动端针对不同主题采用了不同的方案，有些是默认显示，有些不...',
+      '6\n教程笔记\n2020-04-05\nzblog忘记后台账号密码怎么办？\n总有些人账号密码太多，多到自己也记不住，然后来回反复的尝试，还是想不起来，总觉得这个...'
+    ]
+
+    const listArr = list.map((item) => {
+      const [id, type, time, title, content] = item.split('\n')
+
+      return {
+        id,
+        title,
+        time,
+        type,
+        content
+      }
+    })
+
+    console.log(listArr)
+
+    return {
+      listArr
+    }
   }
 })
 </script>

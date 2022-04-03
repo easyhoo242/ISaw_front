64cbc507e2a30e8d15bd5183e04d8ef321d76cf7
Du Cheng <easyhoo2426@qq.com>
2022-04-04 07:05:47 +0800

    feat(sideBar): 侧边栏增加组件注释 1

--- src/components/global/sliderBar/index.vue
+++ src/components/global/sliderBar/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="w-full">
+    <!-- 头像 -->
     <HeadLogo />
+    <!-- 随便看看 -->
     <Casual />
+    <!-- 热门文章 -->
     <HotSee />
+    <!-- 时间线热门评论 -->
     <TimeLine />
   </div>
 </template>

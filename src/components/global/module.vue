884d308f456a97322425adf33ea761e0c9c084b7
Du Cheng <easyhoo2426@qq.com>
2022-04-04 06:50:54 +0800

    feat(module): 修改module名字为Module

--- src/components/global/module.vue
+++ src/components/global/module.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="module p-15px bg-white dark:bg-gray-900 rounded-lg shadow-md p-4">
     <span v-if="isShowCorner" class="module-span"></span>
-    <div v-if="title" class="title text-lg mx-2.5 pb-2.5 font-bold border-b-1">{{ title }}</div>
+    <div v-if="title" class="title text-lg mx-2.5 pb-2.5 font-bold border-b-1">
+      {{ title }}
+    </div>
     <slot name="default" />
   </div>
 </template>

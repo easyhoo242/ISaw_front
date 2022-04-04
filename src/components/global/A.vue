acc15563f293540278d7ff28c5352ec7826d0e1b
Du Cheng <easyhoo2426@163.com>
2022-04-04 23:46:37 +0800

    feat(A): 提取A组件

---
+++ src/components/global/A.vue
@@ -0,0 +1,27 @@
+<template>
+  <a :href="href" :target="target">
+    <slot name="default"></slot>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    href: {
+      type: String,
+      default: ''
+    },
+    target: {
+      type: String,
+      default: ''
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>

0fb33d9c25a8d2a570ad9e5af719c1e5abbcaf58
Du Cheng <easyhoo2426@163.com>
2022-04-04 21:07:18 +0800

    feat(active-top): 布局

--- src/components/global/ActiveTop.vue
+++ src/components/global/ActiveTop.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="active-top h-103 bg-gray-400">fullpage</div>
+  <div
+    class="active-top h-103 bg-gray-400 flex items-center justify-center"
+    :style="`background: url(${bgList.talk.url})`"
+  >
+    <section
+      class="m-auto w-100 h-12 flex items-center justify-center rounded-full"
+    >
+      <span class="font-bold">
+        {{ bgList.talk.title }}
+      </span>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,9 +18,45 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
-    return {}
+    return {
+      bgList: {
+        talk: {
+          title: '闲言碎语',
+          url: 'https://cn.bing.com/th?id=OHR.LoughriggTarn_ZH-CN1404327665_1920x1080.jpg'
+        },
+        seesee: {
+          title: '随便看看',
+          url: 'https://cn.bing.com/th?id=OHR.LoughriggTarn_ZH-CN1404327665_1920x1080.jpg'
+        }
+      }
+    }
   }
 })
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.active-top {
+  background-position-y: center center !important;
+  // background-size: 100% auto !important;
+
+  &:hover {
+    section {
+      background-color: white;
+      color: #000;
+      transition: all 0.5s;
+    }
+  }
+
+  section {
+    color: white;
+    border: 1px solid white;
+    transition: all 0.5s;
+
+    span {
+      user-select: none;
+      font-size: 17.5px;
+      letter-spacing: 0.5em;
+    }
+  }
+}
+</style>

58c05c8e72081b049a8430299afeace636f32371
Du Cheng <easyhoo2426@163.com>
2022-04-05 02:18:18 +0800

    feat(subject): 专题页面

---
+++ src/components/page/subject/SubjectList.vue
@@ -0,0 +1,85 @@
+<template>
+  <div
+    v-for="item in data"
+    :key="item.slogan.id"
+    class="rounded-md bg-white overflow-hidden"
+  >
+    <div
+      class="header h-64 bg-gray-300 mb-4 bg-cover"
+      :style="`background: url(${item.slogan.url})`"
+    />
+    <div class="body px-4 pt-4 pb-3">
+      <div class="body-title mt-5px mb-10px text-xs text-gray-400">
+        {{ item.slogan.title }}
+        <span> · {{ item.slogan.count }} 篇文章</span>
+      </div>
+
+      <div class="body-content">
+        <div
+          v-for="listItem in item.list"
+          :key="listItem.id"
+          class="content-item flex justify-between items-center"
+        >
+          <A>
+            <div class="content-item-title py-5px">{{ listItem.title }}</div>
+          </A>
+          <div
+            class="content-item-type text-xs px-2 bg-gray-100 flex items-center justify-center rounded-sm"
+          >
+            {{ listItem.type }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+interface IDataList {
+  slogan: {
+    id: string
+    title: string
+    count: string
+    url: string
+  }
+  list: {
+    id: number
+    title: string
+    type: string
+  }[]
+}
+;[]
+
+export default defineComponent({
+  props: {
+    data: {
+      type: Array as PropType<IDataList[]>,
+      default: () => []
+    }
+  },
+  setup() {}
+})
+</script>
+<style lang="less" scoped>
+.content-item {
+  .content-item-title {
+    &:before {
+      content: '';
+      width: 10px;
+      height: 10px;
+      border: 2px solid #448ef6;
+      border-radius: 8px;
+      margin: 0 10px 0 0;
+      vertical-align: 0.1em;
+      display: inline-block;
+    }
+  }
+
+  .content-item-type {
+    height: 24px;
+    cursor: pointer;
+  }
+}
+</style>
--- src/pages/subject/index.vue
+++ src/pages/subject/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>{{}}</div>
+  <div>
+    <BranchCrumb route="专题目录" />
+
+    <div class="grid grid-cols-2 gap-4">
+      <SubjectList :data="dataList" />
+    </div>
+  </div>
 </template>
 
 <route lang="yaml">
@@ -12,9 +18,55 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
-    return {}
+    const data = {
+      slogan: {
+        id: '1',
+        title: '这是关于百度 文章的专题栏目，更多更详细的内容请点击查看详情',
+        count: '98',
+        url: 'https://www.talklee.com/zb_users/upload/special/1.jpg'
+      },
+      list: [
+        {
+          id: 1,
+          title:
+            '教程笔记戴尔笔记本电脑插上耳机没有声音，但外放正常耳机好使，怎么回事？',
+          type: '互联网'
+        },
+        {
+          id: 2,
+          title: '互联网聊聊百度智能小程序关联H5站点及小程序改名的心酸历程',
+          type: '互联网'
+        },
+        {
+          id: 3,
+          title: '教程笔记银河麒麟linux系统服务器忘记root密码重置的图文教程',
+          type: '教程笔记'
+        },
+        {
+          id: 4,
+          title: '互联网nofollow标签的作用和使用方法有了重大变化',
+          type: '互联网'
+        },
+        {
+          id: 5,
+          title:
+            '互联网为促进资源下载站点的良性发展，百度搜索资源平台即将上线清风4',
+          type: '闲言碎语'
+        },
+        {
+          id: 6,
+          title:
+            '互联网国企不得续签第三方云平台，多地布局“国资云”，云储存市场大变局',
+          type: '博客模板'
+        }
+      ]
+    }
+
+    const dataList = new Array(7).fill(null).map((res) => data)
+
+    return {
+      dataList
+    }
   }
 })
 </script>
-
-<style scoped></style>

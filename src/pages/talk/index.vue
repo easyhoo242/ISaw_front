bb1794c6450605f3ec47ddc82c804d2ea5574fe8
Du Cheng <easyhoo2426@163.com>
2022-04-04 22:21:37 +0800

    feat(talk): 增加分页

--- src/pages/talk/index.vue
+++ src/pages/talk/index.vue
@@ -3,6 +3,15 @@
   <div class="module-list grid grid-cols-4 gap-3">
     <TalkItem v-for="item in 12" :key="item" />
   </div>
+
+  <a-pagination
+    v-model="currentPage"
+    :default-current="2"
+    :total="200"
+    show-quick-jumper
+    class="mt-4"
+    @change="onChange"
+  ></a-pagination>
 </template>
 
 <route lang="yaml">
@@ -11,12 +20,17 @@ meta:
 </route>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: '闲言碎语',
   setup() {
-    return {}
+    const currentPage = ref(1)
+
+    return {
+      currentPage,
+      onChange() {}
+    }
   }
 })
 </script>

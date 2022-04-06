ae2f567e031ef44c80580558c4f959dee3584980
Du Cheng <easyhoo2426@163.com>
2022-04-07 00:59:30 +0800

    feat(blog): 新增博文详情页面

---
+++ src/pages/blog/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <BranchCrumb route="正文" />
+  <Module> 123 </Module>
+  <FlexCol>
+    <template #body> <HotList></HotList></template>
+
+    <template #side>
+      <Sidebar>
+        <Casual />
+      </Sidebar>
+    </template>
+  </FlexCol>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BranchCrumb from '~/components/global/BranchCrumb.vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+  components: { BranchCrumb }
+})
+</script>
+
+<style lang="less" scoped></style>

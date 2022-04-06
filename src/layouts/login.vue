b9c542475ccf439f2a2a817d58eae50bac4083f7
Du Cheng <easyhoo2426@163.com>
2022-04-06 17:36:24 +0800

    feat(layout): 登录页面布局

---
+++ src/layouts/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <main
+    class="w-full min-h-full flex flex-col items-center dark:bg-black text-gray-700 dark:text-gray-200 overflow-none"
+  >
+    <div class="flex-1 w-full min-h-0">
+      <article class="w-full h-full dark:bg-gray-800">
+        <router-view v-slot="{ Component }">
+          <transition
+            name="fade"
+            enter-active-class="transition easing-entrance duration-250"
+            enter-from-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition easing-exit duration-150"
+            leave-from-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95"
+          >
+            <component :is="Component" />
+          </transition>
+        </router-view>
+      </article>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background: url(~/assets/img/login-bg.jpeg);
+  background-size: 100% 100%;
+  background-position: cover;
+
+  animation: bgsize linear 20s infinite;
+}
+
+@keyframes bgsize {
+  0% {
+    background-size: 100% 100%;
+  }
+  50% {
+    background-size: 110% 110%;
+  }
+  100% {
+    background-size: 100% 100%;
+  }
+}
+</style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>123</div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: login
+</route>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>

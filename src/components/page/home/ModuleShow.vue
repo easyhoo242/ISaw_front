a9bb104a8e6ac33b4403cc9f8b2a75b23635f4a8
Du Cheng <easyhoo2426@163.com>
2022-04-16 06:34:14 +0800

    feat: 修改静态模块的背景图与文案

--- src/components/page/home/ModuleShow.vue
+++ src/components/page/home/ModuleShow.vue
@@ -4,14 +4,12 @@
       <div
         class="left-tag bg-gray-900 text-white w-25 h-8 rounded-full flex items-center justify-center"
       >
-        博客模板
+        作品展示
       </div>
       <div
         class="bg-gray-900 bg-opacity-30 h-full flex items-center justify-center"
       >
-        <div class="left-content mx-6 text-white">
-          博客提供zblog建站、个人博客主题模板、插件制作等一站式服务，支持主题模板的订制和仿站等服务。
-        </div>
+        <div class="left-content mx-6 text-white">将有意思的案例放在这里以供学习和分享</div>
       </div>
     </div>
     <div
@@ -48,51 +46,45 @@ export default defineComponent({
       list: [
         {
           id: 1,
-          title:
-            'ZBP仿月光博客主题模板简风 ，非卖品目前仅供超级会员免费下载使用',
+          title: '[HTML+CSS]【撸完即巅峰】倔强青铜和最强王者之间就差这个Demo',
           time: '21-12-07',
-          author: '李洋博客',
-          url: 'https://www.talklee.com/zb_users/upload/2020/12/202012281609126120338334.jpg'
+          author: 'ISaw',
+          url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg1.doubanio.com%2Fview%2Fnote%2Fl%2Fpublic%2Fp82460877.jpg&refer=http%3A%2F%2Fimg1.doubanio.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652653184&t=a59635edb652c7ee143bb09a52221652'
         },
         {
           id: 2,
-          title:
-            'ZBP仿月光博客主题模板简风 ，非卖品目前仅供超级会员免费下载使用',
+          title: '[HTML+CSS]【撸完即巅峰】倔强青铜和最强王者之间就差这个Demo',
           time: '21-12-07',
-          author: '李洋博客',
-          url: 'https://www.talklee.com/zb_users/upload/2021/06/202106181623980863794781.jpg'
+          author: 'ISaw',
+          url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2018-12-26%2F5c23275600250.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652653451&t=e6fcc82226e805c667bd9defc17a2b91'
         },
         {
           id: 3,
-          title:
-            'ZBP仿月光博客主题模板简风 ，非卖品目前仅供超级会员免费下载使用',
+          title: '[HTML+CSS]【撸完即巅峰】倔强青铜和最强王者之间就差这个Demo',
           time: '21-12-07',
-          author: '李洋博客',
-          url: 'https://www.talklee.com/zb_users/upload/2020/12/202012281609126120338334.jpg'
+          author: 'ISaw',
+          url: 'https://img1.baidu.com/it/u=4097408996,1673831050&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500'
         },
         {
           id: 4,
-          title:
-            'ZBP仿月光博客主题模板简风 ，非卖品目前仅供超级会员免费下载使用',
+          title: '[HTML+CSS]【撸完即巅峰】倔强青铜和最强王者之间就差这个Demo',
           time: '21-12-07',
-          author: '李洋博客',
-          url: 'https://www.talklee.com/zb_users/upload/2021/04/202104191618816131846144.jpg'
+          author: 'ISaw',
+          url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
         },
         {
           id: 5,
-          title:
-            'ZBP仿月光博客主题模板简风 ，非卖品目前仅供超级会员免费下载使用',
+          title: '[HTML+CSS]【撸完即巅峰】倔强青铜和最强王者之间就差这个Demo',
           time: '21-12-07',
-          author: '李洋博客',
-          url: 'https://res.talklee.com/upload/2020/07/202007035026_9675.png'
+          author: 'ISaw',
+          url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
         },
         {
           id: 6,
-          title:
-            'ZBP仿月光博客主题模板简风 ，非卖品目前仅供超级会员免费下载使用',
+          title: '[HTML+CSS]【撸完即巅峰】倔强青铜和最强王者之间就差这个Demo',
           time: '21-12-07',
-          author: '李洋博客',
-          url: 'https://res.talklee.com/upload/2020/06/202006128653_3853.png'
+          author: 'ISaw',
+          url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2018-12-26%2F5c23275600250.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652653451&t=e6fcc82226e805c667bd9defc17a2b91'
         }
       ]
     }
@@ -103,7 +95,7 @@ export default defineComponent({
 <style lang="less" scoped>
 .module-show-left {
   position: relative;
-  background: url('https://www.talklee.com/zb_users/upload/2022/02/202202181645151930842963.png')
+  background: url('https://img1.baidu.com/it/u=4097408996,1673831050&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500')
     no-repeat;
 
   .left-tag {

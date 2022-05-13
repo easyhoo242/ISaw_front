c8c049e7951b28f769f6604b77d9255a173d7443
Du Cheng <easyhoo2426@163.com>
2022-05-13 18:55:15 +0800

    feat(user): 用户信息界面列表调整

--- src/pages/user/[id].vue
+++ src/pages/user/[id].vue
@@ -3,8 +3,53 @@
     <FlexCol>
       <template #body>
         <div class="hotlist flex flex-col">
-          <Blog v-if="list?.length || 0" :data="list" />
+          <div v-if="list?.length">
+            <Module
+              v-for="item in list"
+              :key="item.momentId"
+              class="module flex"
+            >
+              <div
+                class="logo flex-0 w-210px h-158px rounded-md overflow-hidden mr-5"
+              >
+                <A :href="`/blog/${item.momentId}`">
+                  <!-- <img :src="item.images ? item.images[0] : BASE_LOGO" alt="" /> -->
+                  <img :src="BASE_LOGO" alt="" />
+                </A>
+              </div>
 
+              <div class="hotlist-content flex-1 pr-3">
+                <div class="title flex items-center text-gray-700">
+                  <div v-if="item.agree > 20" class="hot-tag mr-2">热文</div>
+                  <A :href="`/blog/${item.momentId}`">
+                    <div class="text-lg">
+                      {{ item.title || '这个人也太懒了吧 居然忘了取标题' }}
+                    </div>
+                  </A>
+                </div>
+
+                <div class="content text-sm my-3 text-gray-500 min-h-77px">
+                  {{ item.content || '这是内容' }}
+                </div>
+
+                <div
+                  class="footer pt-2 flex items-center justify-between text-gray-400"
+                >
+                  <div class="tag flex-1 flex items-center">
+                    <div>🕒 {{ item?.createTime?.split('T')[0] }}</div>
+                    <div>{{ item.agree }} 点赞</div>
+                    <div>💬 {{ item.commentCount }} 评论</div>
+                  </div>
+                  <div class="who flex-0">
+                    <!-- <a-icon></a-icon> -->
+                    <A :href="`/user/${item?.author?.id}`">
+                      🏆 {{ item?.author?.nickname }}
+                    </A>
+                  </div>
+                </div>
+              </div>
+            </Module>
+          </div>
           <Module v-else>
             <a-empty :image="simpleImage" />
           </Module>
@@ -34,9 +79,10 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import { requestMomentById } from '~/api'
-import type { IMomentType } from '~/api'
 import cache from '~/utils/cache'
+import { requestMomentById, BASE_LOGO } from '~/api'
+import type { IMomentType } from '~/api'
+
 import { Empty } from 'ant-design-vue'
 import Module from '~/components/global/Module.vue'
 import ChangeInfo from '~/components/page/user/ChangeInfo.vue'
@@ -77,6 +123,7 @@ export default defineComponent({
     getData()
 
     return {
+      BASE_LOGO,
       isShowChangeInfo,
       list,
       currentPage,
@@ -94,5 +141,41 @@ export default defineComponent({
 .user-center {
   background: transparent !important;
   box-shadow: none !important;
+}
+
+.module {
+  .logo {
+    img {
+      max-width: 100%;
+      height: 100%;
+    }
+  }
+
+  .hotlist-content {
+    .content {
+      line-height: 1.8 !important;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 3;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+
+    .footer {
+      .tag {
+        div {
+          margin-right: 16px;
+        }
+      }
+    }
+  }
+}
+
+.hot-tag {
+  background-color: #ff9627;
+  border-radius: 2px;
+  color: #fff;
+  font-size: 12px;
+  padding: 2px 4px;
 }
 </style>

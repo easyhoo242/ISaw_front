3d1a36182743d25682be1918e5a8668ff674a721
Du Cheng <easyhoo2426@163.com>
2022-05-15 14:19:06 +0800

    feat(header): 标题位置修改

--- src/components/global/Header/index.vue
+++ src/components/global/Header/index.vue
@@ -31,19 +31,19 @@
                 </span>
               </template>
             </a-tab-pane>
+            <a-tab-pane key="/talk">
+              <template #tab>
+                <span class="py-2">
+                  <gg:menu-boxed class="mr-2" />
+                  <strong class="align-top">闲言碎语</strong>
+                </span>
+              </template>
+            </a-tab-pane>
             <a-tab-pane key="/subject">
               <template #tab>
                 <span class="py-2">
                   <gg:menu-boxed class="mr-2" />
                   <strong class="align-top">专题</strong>
-                </span>
-              </template>
-            </a-tab-pane>
-            <a-tab-pane key="/talk">
-              <template #tab>
-                <span class="py-2">
-                  <gg:menu-boxed class="mr-2" />
-                  <strong class="align-top">随便看看</strong>
                 </span>
               </template>
             </a-tab-pane>

7fbed4d74eae363e020ea02bd5f73763e2a35006
Du Cheng <easyhoo2426@163.com>
2022-05-19 02:58:03 +0800

    feat(blog): 博客简介

---
+++ src/components/page/blog/AboutISaw.vue
@@ -0,0 +1,13 @@
+<template>
+  <Module title="博客简介">
+    <div class="p-5 mb-3 pt-3 font-extralight" style="font-size: 15px">
+      <b> ISaw </b>
+      是基于 Node.js 的个人博客系统，采用 Vite + Vue + TypeScript + NodeJs +
+      MySql 技术栈。
+    </div>
+  </Module>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/pages/blog/[id].vue
+++ src/pages/blog/[id].vue
@@ -27,6 +27,8 @@
       </template>
 
       <template #side>
+        <AboutISaw />
+
         <Casual />
 
         <!-- 文章操作模块 -->
@@ -58,6 +60,8 @@ import {
 } from '~/api'
 import type { ICommentType, IResponsType } from '~/api'
 import { message, Modal } from 'ant-design-vue'
+import Module from '~/components/global/Module.vue'
+import AboutISaw from '../../components/page/blog/AboutISaw.vue'
 
 export interface headerInfo {
   id: number
@@ -78,10 +82,11 @@ export default defineComponent({
   setup() {
     const router = useRouter()
     const user = localCache.getCache('user')
-
     const isShowPermission = ref(false)
-
-    const comment = reactive<{ father: ICommentType[]; son: ICommentType[] }>({
+    const comment = reactive<{
+      father: ICommentType[]
+      son: ICommentType[]
+    }>({
       father: [],
       son: []
     })
@@ -89,26 +94,19 @@ export default defineComponent({
     const currentPage = ref(1)
     const total = ref(0)
     const totalAll = ref(0)
-
     //@ts-ignore
     const headerInfo = ref<headerInfo>({})
-
     const labelInfo = reactive<{
       id: number
       name: string
     }>({ id: 0, name: '' })
-
     const currentContent = ref('')
-
     const momentId = parseInt(window.location.pathname.split('/')[2])
-
     const getData = async () => {
       // 文章详情
       const momentDetail = (await requestMomentDetail(momentId))!
-
       // 正文
       currentContent.value = momentDetail.content
-
       // 表头
       headerInfo.value = {
         ...momentDetail?.author,
@@ -120,32 +118,23 @@ export default defineComponent({
         momentId: momentDetail.momentId,
         look: momentDetail.look
       }
-
       labelInfo.id = momentDetail.label.id
       labelInfo.name = momentDetail.label.name
-
       // 是否显示权限操作按钮
       isShowPermission.value = user.id === momentDetail.author.id
-
       // 一级评论列表
       const res = await requestCommentList(currentPage.value, 5, 1, momentId)
-
       // 二级评论列表
       const resC = await requestCommentListSon()
-
       total.value = res.data?.commentCountNotNull!
       totalAll.value = res.data?.commentCount!
-
       comment.father = res.data?.list!
       comment.son = resC.data!
     }
-
     const handlePageChange = (page: number) => {
       currentPage.value = page
-
       getData()
     }
-
     // 删除文章
     const handleDeleteComment = () => {
       Modal.confirm({
@@ -153,17 +142,13 @@ export default defineComponent({
         okText: '确定',
         okType: 'danger',
         cancelText: '取消',
-
         async onOk() {
           const res = await requestMomentDelete(momentId)
-
           if (!res.flag) {
             message.error('删除失败~', 3)
             return
           }
-
           message.success('删除成功~ 2秒后跳转到首页~', 3)
-
           setTimeout(() => {
             router.push('/')
           }, 1500)
@@ -171,64 +156,50 @@ export default defineComponent({
         onCancel() {}
       })
     }
-
     // 发表评论
     const handleSubmit = async (currntComment: string) => {
       if (!currntComment) {
         return
       }
-
       const { flag, msg } = await requestPostComment(momentId, currntComment)
-
       if (!flag) {
         message.error('评论发表失败~', 3)
         return
       }
-
       message.success(msg, 3)
       // 刷新列表
       getData()
     }
-
     // 回复评论
     const handleReply = async (commentId: number, reply: string) => {
       if (!commentId) {
         return
       }
-
       const { flag, msg } = await requestReplyComment(
         momentId,
         reply,
         commentId
       )
-
       if (!flag) {
         message.error('评论回复失败', 2)
       }
-
       message.success(msg, 3)
       getData()
       // reload()
     }
-
     // 删除评论
     const handleDelete = async (id: number) => {
       const { flag, msg } = await requestCommentDelete(id)
-
       if (!flag) {
         message.error('删除失败', 2)
         return
       }
-
       message.success(msg, 3)
-
       getData()
     }
-
     onMounted(() => {
       getData()
     })
-
     //修改文章
     const handleEditMoment = () => {
       router.push({
@@ -238,20 +209,16 @@ export default defineComponent({
         }
       })
     }
-
     // 点赞响应
     const handleAgreeResponse = (res: IResponsType<string>) => {
       const { msg, flag } = res
-
       if (!flag) {
         message.error(msg, 3)
         return
       }
-
       message.success(msg, 2)
       getData()
     }
-
     return {
       pagesize: 5,
       comment,
@@ -273,7 +240,8 @@ export default defineComponent({
       handleEditMoment,
       handleAgreeResponse
     }
-  }
+  },
+  components: { Module, AboutISaw }
 })
 </script>
 

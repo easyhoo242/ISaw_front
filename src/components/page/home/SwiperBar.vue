e4c21809a425cf54930d507a1265c1a91065e0d4
Du Cheng <easyhoo2426@163.com>
2022-05-22 09:06:59 +0800

    feat(swiper): 替换掉轮播图

--- src/components/page/home/SwiperBar.vue
+++ src/components/page/home/SwiperBar.vue
@@ -13,26 +13,43 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { requestMomentDetail } from '~/api'
 const onChange = () => {}
 
-const list = [
-  {
-    id: 1,
-    url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
-  },
-  {
-    id: 2,
-    url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
-  },
-  {
-    id: 3,
-    url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
-  },
-  {
-    id: 5,
-    url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
-  }
-]
+const list = ref<{ id?: number; url?: string }[]>([])
+
+async function getData() {
+  const res = await requestMomentDetail(99)
+
+  list.value = res.images.map((item, index) => ({
+    id: index,
+    url: item
+  }))
+}
+
+onMounted(() => {
+  getData()
+})
+
+// const list = [
+//   {
+//     id: 1,
+//     url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
+//   },
+//   {
+//     id: 2,
+//     url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
+//   },
+//   {
+//     id: 3,
+//     url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
+//   },
+//   {
+//     id: 5,
+//     url: 'https://img0.baidu.com/it/u=1577615142,907299332&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
+//   }
+// ]
 </script>
 
 <style lang="less" scoped>

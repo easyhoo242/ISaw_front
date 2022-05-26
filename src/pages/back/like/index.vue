ba9c32f7cee742d402a332486be9ed306c00c321
Du Cheng <easyhoo2426@163.com>
2022-05-27 04:01:47 +0800

    feat: 点赞记录

--- src/pages/back/like/index.vue
+++ src/pages/back/like/index.vue
@@ -1,36 +1,44 @@
 <template>
-  <a-list item-layout="horizontal" :data-source="data">
-    <template #renderItem="{ item }">
-      <a-list-item>
-        <a-list-item-meta>
-          <template #title>
-            <a :href="`/user/${item.user_id}`">{{ item.author.nickname }}</a>
-          </template>
-          <template #avatar>
-            <a-avatar :src="item.author.avatar || BASE_HEAD_LOGO" />
-          </template>
-          <template #description>
-            <A :href="`/blog/${item.moment_id}`">{{ item.moment.title }}</A>
-          </template>
-        </a-list-item-meta>
-      </a-list-item>
-    </template>
-  </a-list>
+  <div>
+    <a-list item-layout="horizontal" :data-source="data">
+      <template #renderItem="{ item }">
+        <a-list-item>
+          <a-list-item-meta>
+            <template #title>
+              <a :href="`/user/${item.user_id}`">{{ item.author.nickname }}</a>
+            </template>
+            <template #avatar>
+              <a-avatar :src="item.author.avatar || BASE_HEAD_LOGO" />
+            </template>
+            <template #description>
+              <span>点赞了文章：</span>
+              <A :href="`/blog/${item.moment_id}`">
+                {{ item.moment.title }}
+              </A>
+              <span>
+                于<b> {{ item.createTime }}</b>
+              </span>
+            </template>
+          </a-list-item-meta>
+        </a-list-item>
+      </template>
+    </a-list>
 
-  <a-pagination
-    :current="currentPage"
-    :pageSize="20"
-    :total="total"
-    show-quick-jumper
-    hideOnSinglePage
-    class="my-3 text-right"
-    @change="handlePageChange"
-  />
+    <a-pagination
+      :current="currentPage"
+      :pageSize="20"
+      :total="total"
+      show-quick-jumper
+      hideOnSinglePage
+      class="my-3 text-right"
+      @change="handlePageChange"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { requestBackMomentLook, BASE_HEAD_LOGO } from '~/api'
+import { requestBackMomentLike, BASE_HEAD_LOGO } from '~/api'
 import type { IBackLookList } from '~/api'
 
 const data = ref<IBackLookList[]>([])
@@ -44,7 +52,7 @@ const handlePageChange = (page: number) => {
 }
 
 const getData = async () => {
-  const res = await requestBackMomentLook(currentPage.value)
+  const res = await requestBackMomentLike(currentPage.value)
 
   data.value = res.list
   total.value = res.count

44abf7c8400901ed0c038f50ce3743c016811c11
Du Cheng <easyhoo2426@163.com>
2022-05-27 18:13:33 +0800

    feat(message): 留言后台管理 完成

--- src/api/message.ts
+++ src/api/message.ts
@@ -9,14 +9,36 @@ enum MessageApi {
   edit = '/message/' // post  + messageId
 }
 
+export interface IMessageList {
+  id: number
+  content: string
+  score: number
+  createTime: string
+  user: {
+    id: number
+    nickname: string
+    avatarUrl: string
+  }
+}
+
+interface IMessage {
+  list: IMessageList[]
+  count: number
+}
+
+export interface IMessageContent {
+  content: string
+  score: number
+}
+
 export const requestMessageList = () => {
-  return hyRequest.get({
+  return hyRequest.get<IResponsType<IMessage>>({
     url: MessageApi.list
   })
 }
 
 export const requestAddMesage = (content: string, score: number) => {
-  return hyRequest.post({
+  return hyRequest.post<IResponsType<IMessage>>({
     url: MessageApi.add,
     data: {
       content,
@@ -26,13 +48,17 @@ export const requestAddMesage = (content: string, score: number) => {
 }
 
 export const requestDelMesage = (messageId: number) => {
-  return hyRequest.delete({
-    url: MessageApi.list + messageId
+  return hyRequest.delete<IResponsType<IMessage>>({
+    url: MessageApi.del + messageId
   })
 }
 
-export const requestEditMessage = (messageId: number) => {
-  return hyRequest.post({
-    url: MessageApi.list + messageId
+export const requestEditMessage = (
+  messageId: number,
+  data: IMessageContent
+) => {
+  return hyRequest.post<IResponsType<string>>({
+    url: MessageApi.edit + messageId,
+    data
   })
 }
--- src/pages/back/message/index.vue
+++ src/pages/back/message/index.vue
@@ -8,18 +8,12 @@
       :rowClassName="(_:any, index:number) => (index % 2 === 1 ? 'table-striped' : null)"
       bordered
     >
-      <template #type="{ text: type }">
-        <span>
-          <a-tag
-            :color="type === 9 ? 'volcano' : type == 6 ? 'geekblue' : 'green'"
-          >
-            {{ type === 3 ? '普通用户' : type === 6 ? '会员' : '管理员' }}
-          </a-tag>
-        </span>
-      </template>
-
       <template #action="{ record }">
-        <a-button type="default" size="small" @click="handleEdit(record)">
+        <a-button
+          type="default"
+          size="small"
+          @click="handleEdit(record.id, record)"
+        >
           编辑
         </a-button>
 
@@ -27,7 +21,7 @@
           title="确定删除这条数据吗？"
           ok-text="删除"
           cancel-text="取消"
-          @confirm="handleDelete(record)"
+          @confirm="handleDelete(record.id)"
         >
           <a-button type="default" size="small" class="ml-1"> 删除 </a-button>
         </a-popconfirm>
@@ -48,38 +42,18 @@
         :label-col="labelCol"
         :wrapper-col="wrapperCol"
       >
-        <a-form-item label="昵称">
-          <a-input v-model:value="formState.nickname" />
-        </a-form-item>
-        <a-form-item label="手机号码">
-          <a-input v-model:value="formState.telPhone" />
-        </a-form-item>
-        <a-form-item label="邮箱">
-          <a-input v-model:value="formState.email" />
+        <a-form-item label="留言内容">
+          <a-textarea v-model:value="formState.content" />
         </a-form-item>
 
-        <a-form-item label="年龄">
-          <a-input type="number" v-model:value="formState.age" />
-        </a-form-item>
-
-        <a-form-item label="性别">
-          <a-select v-model:value="formState.sex" placeholder="选择性别">
-            <a-select-option :value="'男'">男</a-select-option>
-            <a-select-option :value="'女'">女</a-select-option>
-            <a-select-option :value="'保密'">保密</a-select-option>
+        <a-form-item label="评分">
+          <a-select v-model:value="formState.score" placeholder="修改评分">
+            <a-select-option :value="1">1</a-select-option>
+            <a-select-option :value="2">2</a-select-option>
+            <a-select-option :value="3">3</a-select-option>
+            <a-select-option :value="4">4</a-select-option>
+            <a-select-option :value="5">5</a-select-option>
           </a-select>
-        </a-form-item>
-
-        <a-form-item label="权限级别">
-          <a-select v-model:value="formState.type" placeholder="用户权限级别">
-            <a-select-option :value="3">普通用户</a-select-option>
-            <a-select-option :value="6">会员</a-select-option>
-            <a-select-option :value="9">管理员</a-select-option>
-          </a-select>
-        </a-form-item>
-
-        <a-form-item label="签名">
-          <a-textarea v-model:value="formState.desc"></a-textarea>
         </a-form-item>
       </a-form>
     </a-modal>
@@ -89,8 +63,8 @@
 <script lang="ts">
 import { defineComponent, ref, reactive, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
-import { requestUserList, changeUserInfo, requestdeleteUser } from '~/api'
-import type { IUserInfoType, IUser } from '~/api'
+import { requestMessageList, requestDelMesage, requestEditMessage } from '~/api'
+import type { IMessageContent, IMessageList } from '~/api'
 import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue'
 import { message } from 'ant-design-vue'
 
@@ -101,50 +75,25 @@ const columns = [
     key: 'id'
   },
   {
-    title: '用户名',
-    dataIndex: 'username',
-    key: 'username'
-  },
-  {
-    title: '昵称',
-    dataIndex: 'nickname',
+    title: '留言者',
+    dataIndex: 'user.nickname',
     key: 'nickname'
   },
   {
-    title: '性别',
-    dataIndex: 'sex',
-    key: 'sex'
+    title: '留言内容',
+    dataIndex: 'content',
+    key: 'content',
+    width: '40%'
   },
   {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age'
+    title: ' 评分',
+    dataIndex: 'score',
+    key: 'score'
   },
   {
-    title: '手机号码',
-    dataIndex: 'telPhone',
-    key: 'telPhone'
-  },
-  {
-    title: '邮箱',
-    dataIndex: 'email',
-    key: 'email'
-  },
-  {
-    title: '权限',
-    dataIndex: 'type',
-    key: 'type',
-    slots: { customRender: 'type' }
-  },
-  {
-    title: '创建时间',
+    title: '留言时间',
     dataIndex: 'createTime',
     key: 'createTime'
-  },
-  {
-    title: '更新时间',
-    dataIndex: 'updateTime',
-    key: 'updateTime'
   },
   {
     title: '操作',
@@ -156,7 +105,7 @@ const columns = [
   align: 'center'
 }))
 
-const data = ref<IUserInfoType[]>([])
+const data = ref<IMessageList[]>([])
 
 export default defineComponent({
   components: {
@@ -171,18 +120,16 @@ export default defineComponent({
     const currentUser = ref(0)
 
     const getData = async () => {
-      const res = await requestUserList()
+      const res = await requestMessageList()
 
       // @ts-ignore
-      data.value = res.userList.map((res) => ({
-        ...res
-      }))
-      total.value = res.count!
+      data.value = res.data?.list!
+      total.value = res.data?.count!
     }
 
     const handleAddUser = () => {
       const routeUrl = router.resolve({
-        path: '/register'
+        path: '/message'
       })
 
       window.open(routeUrl.href, '_blank')
@@ -191,21 +138,17 @@ export default defineComponent({
     // 修改用户
     const visibleEdit = ref<boolean>(false)
 
-    const formState = reactive<IUser>({
-      nickname: '',
-      telPhone: '',
-      email: '',
-      sex: '',
-      type: 3,
-      desc: '',
-      age: 18
+    const formState = reactive<IMessageContent>({
+      content: '',
+      score: 1
     })
 
     const handleOkEdit = async () => {
       visibleEdit.value = false
       // @ts-ignore
-      const res = await changeUserInfo(currentUser.value, formState)
+      const res = await requestEditMessage(currentUser.value, formState)
 
+      console.log(res)
       if (!res.flag) {
         message.error(res.msg, 3)
         return
@@ -216,24 +159,18 @@ export default defineComponent({
       getData()
     }
 
-    const handleEdit = async (data: IUserInfoType) => {
+    const handleEdit = async (id: number, data: IMessageContent) => {
       visibleEdit.value = true
 
-      currentUser.value = data.id
+      currentUser.value = id
 
-      formState.nickname = data.nickname
-      formState.telPhone = data.telPhone
-      formState.email = data.email
-      formState.sex = data.sex
-      formState.age = data.age
-      formState.type = data.type
-      formState.desc = data.desc
+      formState.content = data.content
+      formState.score = data.score
     }
 
-    // 删除用户
-    const handleDelete = async (user: IUserInfoType) => {
-      console.log(user)
-      const res = await requestdeleteUser(user.id)
+    // 删除留言
+    const handleDelete = async (id: number) => {
+      const res = await requestDelMesage(id)
 
       if (!res.flag) {
         message.error(res.msg, 3)

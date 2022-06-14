975515409baf6bc1bc62c6862259e2655de1fce4
Du Cheng <easyhoo2426@163.com>
2022-06-14 21:49:29 +0800

    feat(socket): 修改请求地址

--- src/hooks/websocket/config.ts
+++ src/hooks/websocket/config.ts
@@ -1,11 +1,11 @@
 let BASE_URL = 'localhost:8000'
 
 if (process.env.NODE_ENV === 'development') {
-  BASE_URL = 'http://localhost:8000'
+  BASE_URL = 'localhost:8000'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://120.48.82.2:8005'
+  BASE_URL = '120.48.82.2:8005'
 } else {
-  BASE_URL = 'http://120.48.82.2:8005'
+  BASE_URL = '120.48.82.2:8005'
 }
 
 
